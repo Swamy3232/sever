33c7aff45d7655ad0c4e077fea5952ae7a441177
Swamy3232 <swamiaws85@gmail.com>
2025-09-21 13:48:41 +0530

    d

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { Client } from 'pg';
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 
 const app = express();
 app.use(cors());
